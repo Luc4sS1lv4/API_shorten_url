db87c9f1c30c0f2328c0462c7b3c8ef5899078aa
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-09-11 01:16:42 -0300

    the_process

--- src/routers/router.shorten.ts
+++ src/routers/router.shorten.ts
@@ -3,17 +3,19 @@ import { nanoid } from "nanoid";
 
 
 export function RoutersShots(fastify: FastifyInstance){
-    const db: Array<any> | any = []
+    const db: any = []
    
    
     fastify.post<{Body: any}>("/shorten", (req, rep)=>{
         const url:any = req.body
         const newURL = nanoid(6)
-        db.push({newURL, url})
+        url.new = newURL 
+        db.push(url)
         rep.send(db)
     })
 
-    fastify.get<{Params: any | []}>("/localhost:8080/:`${db.newURL}`", (req, rep)=>{
-        rep.redirect(db)
+    fastify.get<{Params: any | []}>("/localhost:8080/:{{db.newURL}}", (req, rep)=>{
+        const urloriginal = db.newURL
+        rep.redirect(urloriginal)
     })
 }
