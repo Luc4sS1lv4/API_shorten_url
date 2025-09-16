ba93268e328f819997b00fc11a40679a8de08e34
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-09-15 23:59:01 -0400

    AjustadInterLinks

--- src/interfaces/IFindShorten.ts
+++ src/interfaces/IFindShorten.ts
@@ -1,3 +1,6 @@
+import type { IShorten } from "./IShorten.js";
+
 export interface IFindDbInMemory{
-    FindMemory(): string & number
+    FindMemory(newUrl: string): IShorten | undefined
+    create(UrlOriginal: string, NewUrl: string, Clciks: number): IShorten
 }
