e4911cf70193685c9d093d759ab500bff35c8945
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2025-09-19 11:23:25 -0400

    add Expire in link

--- src/services/link.create.ts
+++ src/services/link.create.ts
@@ -9,14 +9,13 @@ export class LinkCreate {
         const URL = this.ReposiLink.FindMemory(UrlOrigin)
         if (URL) throw new Error("O link ja foi incurtado antes, selecione um novo link")
         const newl = nanoid(6)
-        const Experin = 60 * 60 * 1000
         const agora = new Date()
         atDate = new Date(agora)
         const future = atDate.setHours(atDate.getHours() + 1)
         Clicks = 0
         NewUrl = newl
-        if (atDate.getTime() == future) throw new Error ("Link expirado")
-            
-            return this.ReposiLink.create(UrlOrigin, NewUrl, Clicks, atDate)
+        if (atDate.getTime() == future) throw new Error("Link expirado")
+
+        return this.ReposiLink.create(UrlOrigin, NewUrl, Clicks, atDate)
     }
 }
